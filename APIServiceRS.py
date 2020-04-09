2f6e0893737cb11f7ed1d11cc7a83c320610731c
jppianta <joao.pianta@outlook.com>
2020-04-09 16:40:08 -0300

    add casos de morte

--- APIServiceRS.py
+++ APIServiceRS.py
@@ -1,17 +1,36 @@
 import requests
 from datetime import date
 
+
 class APIServiceRS:
   def __init__(self):
-    self.base_url = "https://hsig.sema.rs.gov.br/arcgis/rest/services/COVID"
-    self.data = None
+    self.__base_url = "https://hsig.sema.rs.gov.br/arcgis/rest/services/COVID"
+    self.__confirmed_data = None
+    self.__mortes_data = None
 
-  def __request_data(self):
-    params = {"f": "json", "where": "1=1", "returnGeometry": "false", "outFields": "*"}
+  def __request_historico_confirmados(self):
+    params = {
+      "f": "json",
+      "where": "1=1",
+      "returnGeometry": "false",
+      "outFields": "*"
+    }
     response = requests.post(url="%s/Casos_hist/FeatureServer/0/query" %
-                  (self.base_url), params=params)
-    self.data = response.json()
+                             (self.__base_url),
+                             params=params)
+    self.__confirmed_data = response.json()
 
+  def __request_historico_mortes(self):
+    params = {
+      "f": "json",
+      "where": "1=1",
+      "returnGeometry": "false",
+      "outFields": "*"
+    }
+    response = requests.post(url="%s/Mortes_hist/FeatureServer/0/query" %
+                             (self.__base_url),
+                             params=params)
+    self.__mortes_data = response.json()
 
   # Retorno: Dicionário de nomes dos municípios mapeado para dicionário com nome, data de atualização e histórico. Histórico é uma lista de dicionários com casos e data em formato date.
   # {
@@ -19,7 +38,7 @@ class APIServiceRS:
   #     nome: str,
   #     atualizado: date,
   #     historico: { casos: int, data: date }[]
-  #   } 
+  #   }
   # }
   def __parse_historico_municipio(self, data):
     result = dict()
@@ -35,20 +54,43 @@ class APIServiceRS:
         if key.startswith("casos_") and value != None:
           data_caso = key.replace("casos_", "")
           data_caso = data_caso.split("_")
-          parsed_data = "2020/%s/%s"%(data_caso[0], data_caso[1])
+          parsed_data = "2020/%s/%s" % (data_caso[0], data_caso[1])
           data_caso = date(2020, int(data_caso[1]), int(data_caso[0]))
-          historico[parsed_data] = { "casos": int(value), "data": data_caso }
-      result[attributes["nm_municip"]] = { "nome": attributes["nm_municip"], "historico": historico, "atualizado": updated }
+          historico[parsed_data] = {"casos": int(value), "data": data_caso}
+      result[attributes["nm_municip"]] = {
+        "nome": attributes["nm_municip"],
+        "historico": historico,
+        "atualizado": updated
+      }
 
     return result
 
-  # Retorna o histórico dos municípios. Dados ficam em cache para evitar requests desnecessárias, mas se precisar refazer, chamar com force_request como True
-  def get_historico_municipio(self, force_request = False):
+  # Retorna o histórico de casos confirmados dos municípios. Dados ficam em cache para evitar requests desnecessárias, mas se precisar refazer, chamar com force_request como True
+  def get_historico_confirmados_municipio(self, force_request=False):
     if force_request:
-      self.__request_data()
-    elif self.data == None:
-      self.__request_data()
-    return self.__parse_historico_municipio(self.data)
+      self.__request_historico_confirmados()
+    elif self.__confirmed_data == None:
+      self.__request_historico_confirmados()
+    return self.__parse_historico_municipio(self.__confirmed_data)
+
+  # Retorna o histórico de casos de morte dos municípios. Dados ficam em cache para evitar requests desnecessárias, mas se precisar refazer, chamar com force_request como True
+  def get_historico_mortes_municipio(self, force_request=False):
+    if force_request:
+      self.__request_historico_mortes()
+    elif self.__mortes_data == None:
+      self.__request_historico_mortes()
+    return self.__parse_historico_municipio(self.__mortes_data)
+
+  # Retorna dicionario com historico de confirmados e mortes
+  def get_historico_completo(self, force_request=False):
+    if force_request:
+      self.__request_historico_confirmados()
+      self.__request_historico_mortes()
+    return {
+      "confirmados": self.get_historico_confirmados_municipio(),
+      "mortes": self.get_historico_mortes_municipio()
+    }
+
 
 # Exemplo de uso:
 # api = APIServiceRS()
